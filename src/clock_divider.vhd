f148677eb4dc97c2f41a969bc20684b70539c998
Marco Manino <mrcmnn@gmail.com>
2015-01-13 14:40:07 +0100

    Altra prova per carlo

--- src/clock_divider.vhd
+++ src/clock_divider.vhd
@@ -3,15 +3,24 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity clock_divider is
-	generic( DIV : positive := 200 );
+	generic(
+		N : positive := 8; -- Number of bits to store the counter
+		DIV : positive := 200 
+	);
 	port (
-		CLK : in std_logic;
-		O_CLK : inout std_logic
+		CLK, RST : in std_logic; -- Clock and Reset needed
+		O_CLK : inout std_logic -- output is feeded as an input, so inout!
 	);
 end clock_divider;
 
 architecture Behavioral of clock_divider is
-	signal cnt : unsigned(
+	signal cnt : unsigned(N-1 downto 0);
 begin
+	-- Sanity check, needed to avoid strange behavior
+	assert ( 2**N > DIV) report "N needs to be bigger to store DIV";
 	
+	process (CLK, RST) 
+	begin
+		
+	end process;
 end architecture;
