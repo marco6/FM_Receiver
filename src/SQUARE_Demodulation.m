480be4947c47250ef43e951f795935c0952a0cd6
marco6 <mrcmnn@gmail.com>
2015-01-21 14:09:34 +0100

    Qualche cagatina... COMUNQUE io consegnerei il repository.. A cazzo duro!

--- src/SQUARE_Demodulation.m
+++ src/SQUARE_Demodulation.m
@@ -8,41 +8,57 @@ close all
 Fs = 1e6;
 
 % Frequenza di modulazione
-Fc = 5000;
+Fc = 50000;
 
 % Trasmetto 1/10 di secondo!
-t = 0:1/Fs:1; 
+t = 0:1/Fs:0.1; 
 
 % Input di prova
-x = sin(2*pi*300*t) + 3 * sin(2*pi*800*t) + 2 * sin(2*pi*600*t) + 3 * sin(2*pi*400*t);
+x = sin(2*pi*800*t);
 x = x/max(x);
 
 
 % deviazione 
-dev = 50;
+dev = 200;
 
 % Modulazione
 y = fmmod(x, Fc, Fs, dev);
 
 % Amplificazione con clipping
-y = (min(1, max(-1, y * 1000)) + 1) / 2;
+y = y > 0;
 
 % segnale demodulazione e risultato
 z = (square(2 * pi * Fc * t) + 1) / 2;
 
-for i = 1:length(y)
-	z(i) = (z(i) ~= y(i));
-end	
-z = -cumsum(z - mean(z));
-z = z/max(z);
+z = z ~= y;
+	
+z = cumsum(z - 0.5);
+%z = z/max(z);
 
-[b, a] = butter(4, 3000/Fs);
+% decimo z
+z_dec = z(1:round(Fs/Fc):length(z));
+t_dec = t(1:round(Fs/Fc):length(t));
 
-z1 = filter(b,a,z);
+%[b, a] = butter(4, 15000/Fs);
+
+z1 = z;%filter(b,a,z);
 
 [b, a] = butter(4, 400/Fs, 'high');
 
 z1 = filter(b,a,z1);
-z1 = z1/max(z1);
-plot(t(1:length(t)-1400), [x(1:length(x)-1400);z1(1401:length(z1))]);
+%z1 = z1/max(z1);
 
+
+
+
+
+%[b, a] = butter(4, 15000/Fc);
+%z2 = filter(b,a,z_dec);
+z2 = z_dec;
+[b, a] = butter(4, 100/Fc, 'high');
+
+z2 = filter(b,a,z2);
+%z2 = z2/max(z2);
+plot(t, [x;z1]);
+hold on
+plot(t_dec, z2, 'r');
